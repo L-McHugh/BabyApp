3e3c919e67eaf9228d04f0433b8938f3cbd1e812
L-McHugh <l.mchugh1@outlook.com>
2023-01-01 12:30:38 +0000

    Error handling

--- Backend/routes/feeding_routes.js
+++ Backend/routes/feeding_routes.js
@@ -9,38 +9,66 @@ import {
   deleteFeeding,
 } from "../models/feeding_models.js";
 
+// GET /feeding with error handling for invalid ID
 feedingRouter.get("/:id", async function (req, res) {
   const babyID = req.params.id;
   const babyInfo = await getFeedingInfoByID(babyID);
-  console.log(babyInfo);
-  return res.json({ success: true, payload: babyInfo });
+  // babyInfo.length === 0 because if the ID is invalid, it will return an empty array
+  if (babyInfo.length === 0) {
+    return res.json({ success: false, payload: "Invalid ID" });
+  } else {
+    return res.json({ success: true, payload: babyInfo });
+  }
 });
 
-feedingRouter.get("/:id/:volume", async function (req, res) {
-  const babyID = req.params.id;
-  const volume = req.params.volume;
-  const babyInfo = await getFeedingInfoByVolume(volume, babyID);
-  return res.json({ success: true, payload: babyInfo });
-});
+// Didn't need in the end as created a progress bar
+// GET /feeding by id and volume with error handling for invalid ID and invalid volume
 
+// feedingRouter.get("/:id/:volume", async function (req, res) {
+//   const babyID = req.params.id;
+//   const volume = req.params.volume;
+//   const babyInfo = await getFeedingInfoByVolume(volume, babyID);
+//   // babyInfo.length === 0 because if the volume is invalid, it will return an empty array
+//   if (babyInfo.length === 0) {
+//     return res.json({ success: false, payload: "Invalid ID or volume" });
+//   } else {
+//     return res.json({ success: true, payload: babyInfo });
+//   }
+// });
+
+
+// POST /feeding with error handling for invalid ID
 feedingRouter.post("/", async function (req, res) {
   const feedingPost = req.body;
   const addFeeding = await addFeedingInfo(feedingPost);
-  return res.json({ success: true, payload: addFeeding });
+  if (addFeeding === "Invalid ID") {
+    return res.json({ success: false, payload: "Invalid ID" });
+  } else {
+    return res.json({ success: true, payload: addFeeding });
+  }
 });
 
+// PATCH /feeding with error handling for invalid ID
 feedingRouter.patch("/:id", async function (req, res) {
-  const id = req.params.id;
+  const feedingID = req.params.id;
   const feedingPatch = req.body;
-  const patchFeedingInfo = await updateFeeding(id, feedingPatch);
-  console.log(patchFeedingInfo);
-  return res.json({ success: true, payload: patchFeedingInfo });
+  const patchFeedingInfo = await updateFeeding(feedingID, feedingPatch);
+  if (patchFeedingInfo === "Invalid ID") {
+    return res.json({ success: false, payload: "Invalid ID" });
+  } else {
+    return res.json({ success: true, payload: patchFeedingInfo });
+  }
 });
 
+// DELETE /feeding with error handling for invalid ID
 feedingRouter.delete("/:id", async function (req, res) {
   const feedingID = req.params.id;
   const deleteFeedingInfo = await deleteFeeding(feedingID);
-  return res.json({ success: true, payload: deleteFeedingInfo });
+  if (deleteFeedingInfo === "Invalid ID") {
+    return res.json({ success: false, payload: "Invalid ID" });
+  } else {
+    return res.json({ success: true, payload: deleteFeedingInfo });
+  }
 });
 
 export default feedingRouter;
--- Backend/routes/nappies_routes.js
+++ Backend/routes/nappies_routes.js
@@ -9,39 +9,66 @@ import {
   deleteNappy,
 } from "../models/nappies_models.js";
 
+// GET / nappies with error handling for invalid ID
 nappiesRouter.get("/:id", async function (req, res) {
   const babyID = req.params.id;
   const nappyInfo = await getNappyInfo(babyID);
-  console.log(nappyInfo);
-  return res.json({ success: true, payload: nappyInfo });
+  // nappyInfo.length === 0 because if the ID is invalid, it will return an empty array
+  if (nappyInfo.length === 0) {
+    return res.json({ success: false, payload: "Invalid ID" });
+  } else {
+    return res.json({ success: true, payload: nappyInfo });
+  }
 });
 
+// GET/ nappies by id and description with error handling for invalid ID and invalid description
 nappiesRouter.get("/:id/:description", async function (req, res) {
   const babyID = req.params.id;
   const description = req.params.description;
   const nappyInfo = await getNappyInfoByDescription(description, babyID);
-  return res.json({ success: true, payload: nappyInfo });
+  // nappyInfo.length === 0 because if the description is invalid, it will return an empty array
+  if (nappyInfo.length === 0) {
+    return res.json({ success: false, payload: "Invalid ID or description" });
+  } else {
+    return res.json({ success: true, payload: nappyInfo });
+  }
 });
 
+// POST / nappies with error handling for invalid ID
 nappiesRouter.post("/", async function (req, res) {
   const nappyPost = req.body;
   const addNappy = await addNappyInfo(nappyPost);
-  console.log(addNappy);
-  return res.json({ success: true, payload: addNappy });
+  // addNappy === "Invalid ID" because if the ID is invalid, it will return "Invalid ID"
+  if (addNappy === "Invalid ID") {
+    return res.json({ success: false, payload: "Invalid ID" });
+  } else {
+    return res.json({ success: true, payload: addNappy });
+  }
 });
 
+// PATCH / nappies with error handling for invalid ID
 nappiesRouter.patch("/:id", async function (req, res) {
-  const id = req.params.id;
+  const nappyID = req.params.id;
   const nappyPatch = req.body;
-  const patchNappyInfo = await updateNappy(id, nappyPatch);
-  return res.json({ success: true, payload: patchNappyInfo });
+  const patchNappyInfo = await updateNappy(nappyID, nappyPatch);
+  if (patchNappyInfo === "Invalid ID") {
+    return res.json({ success: false, payload: "Invalid ID" });
+  } else {
+    return res.json({ success: true, payload: patchNappyInfo });
+  }
 });
 
+
+// DELETE / nappies with error handling for invalid ID
 nappiesRouter.delete("/:id", async function (req, res) {
   const nappyID = req.params.id;
   const deleteNappyInfo = await deleteNappy(nappyID);
-  console.log(deleteNappyInfo);
-  return res.json({ success: true, payload: deleteNappyInfo });
+  if (deleteNappyInfo === "Invalid ID") {
+    return res.json({ success: false, payload: "Invalid ID" });
+  } else {
+    return res.json({ success: true, payload: deleteNappyInfo });
+  }
 });
+
 
 export default nappiesRouter;
